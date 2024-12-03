2b0836fa268f2c63343f6c39261a6819be98a275
Fabian Kock <fabian.kock@doubleslash.de>
2024-12-03 14:57:34 +0100

    add task api service class

--- todo-list-api-test/src/test/java/de/doubleslash/todolist/apitest/cucumber/stepdefinitions/TaskApiStepDefinitions.java
+++ todo-list-api-test/src/test/java/de/doubleslash/todolist/apitest/cucumber/stepdefinitions/TaskApiStepDefinitions.java
@@ -24,46 +24,41 @@ public class TaskApiStepDefinitions extends CucumberSpringConfiguration {
 
    private static final Logger logger = LoggerFactory.getLogger(TaskApiStepDefinitions.class);
 
-   @Before
-   public void setUp() {
-      createdTaskIds = new ArrayList<>();
-   }
-
    @Given("{int} open tasks are present")
    public void tasksAreOpen(final int taskCount) {
       for (int i = 0; i < taskCount; i++) {
          final String title = "Task " + i;
-         postResponse = createTask(title, TaskStatus.OPEN);
+         taskApiService.createTask(title, TaskStatus.OPEN);
       }
    }
 
    @Given("api endpoint for adding todos is present")
    public void apiEndpointForAddingTodos() {
-      postResponse = createTask("apiEndpointForAddingTodos", TaskStatus.OPEN);
+      taskApiService.createTask("apiEndpointForAddingTodos", TaskStatus.OPEN);
    }
 
    @When("a task with no title is added")
    public void aTaskWithNoTitleIsAdded() {
-      postResponse = createTask(null, TaskStatus.OPEN);
+      taskApiService.createTask(null, TaskStatus.OPEN);
    }
 
    @When("all tasks are requested")
    public void allTasksAreRequested() {
-      final List<Task> tasks = getAllTasks();
+      final List<Task> tasks = taskApiService.getAllTasks();
       assertThat(tasks).isNotNull();
    }
 
    @When("task should be marked complete")
    public void taskShouldBeMarkedComplete() {
-      for (final Long taskId : createdTaskIds) {
-         markTaskAsDone(taskId);
-         assertThat(patchResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+      for (final Long taskId : taskApiService.getCreatedTaskIds()) {
+         taskApiService.markTaskAsDone(taskId);
+         assertThat(taskApiService.getPatchResponse().getStatusCode()).isEqualTo(HttpStatus.OK);
       }
    }
 
    @Then("there are {int} open tasks")
    public void thereAreAtLeastOpenTasks(final int taskCount) {
-      final long openTasksCount = getCountByTaskStatus(TaskStatus.OPEN);
+      final long openTasksCount = taskApiService.getCountByTaskStatus(TaskStatus.OPEN);
       assertThat(openTasksCount).isEqualTo(taskCount);
    }
 
@@ -71,13 +66,13 @@ public class TaskApiStepDefinitions extends CucumberSpringConfiguration {
    public void theServerShouldRespondWithOnPostEndpoint(final int responseCode, final String endpoint) {
       switch (endpoint) {
       case "POST":
-         assertThat(postResponse.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(responseCode));
+         assertThat(taskApiService.getPostResponse().getStatusCode()).isEqualTo(HttpStatusCode.valueOf(responseCode));
          break;
       case "GET":
-         assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(responseCode));
+         assertThat(taskApiService.getGetResponse().getStatusCode()).isEqualTo(HttpStatusCode.valueOf(responseCode));
          break;
       case "PATCH":
-         assertThat(patchResponse.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(responseCode));
+         assertThat(taskApiService.getPatchResponse().getStatusCode()).isEqualTo(HttpStatusCode.valueOf(responseCode));
          break;
       default:
          logger.error("Invalid endpoint: {}", endpoint);
@@ -87,29 +82,29 @@ public class TaskApiStepDefinitions extends CucumberSpringConfiguration {
 
    @Then("the server should respond with {int} on GET endpoint")
    public void theServerShouldRespondWith(final int responseCode) {
-      assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(responseCode));
+      assertThat(taskApiService.getGetResponse().getStatusCode()).isEqualTo(HttpStatusCode.valueOf(responseCode));
    }
 
    @Then("the server should respond with {int} on PATCH endpoint")
    public void theServerShouldRespondWithOnPATCHEndpoint(final int responseCode) {
-      assertThat(patchResponse.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(responseCode));
+      assertThat(taskApiService.getPatchResponse().getStatusCode()).isEqualTo(HttpStatusCode.valueOf(responseCode));
    }
 
    @Then("there are no open tasks")
    public void thereAreNoOpenTasks() {
-      final long countOpenTasks = getCountByTaskStatus(TaskStatus.OPEN);
+      final long countOpenTasks = taskApiService.getCountByTaskStatus(TaskStatus.OPEN);
       assertThat(countOpenTasks).isZero();
    }
 
    @Then("there are {int} completed tasks")
    public void thereAreCompletedTasks(final int taskCount) {
-      final long countDoneTasks = getCountByTaskStatus(TaskStatus.DONE);
+      final long countDoneTasks = taskApiService.getCountByTaskStatus(TaskStatus.DONE);
       assertThat(countDoneTasks).isEqualTo(taskCount);
    }
 
    @After
    public void cleanup() {
-      markAllTasksAsDone();
-      createdTaskIds.clear();
+      taskApiService.markAllTasksAsDone();
+      taskApiService.getCreatedTaskIds().clear();
    }
 }
